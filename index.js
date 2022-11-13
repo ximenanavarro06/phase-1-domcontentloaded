71ef4449bc8bf91aa3a1c2ce28956ea6e6e3a853
ximenanavarro <ximenanavarro@Ximenas-MacBook-Pro.local>
2022-11-13 16:48:36 -0700

    commit tree 13fdf1745288a405b822601f4c4e515d01bfd5e5

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+});
+
+function updateDOM() {
+    document.getElementById("text")
+    .textContent = "This is really cool!";
+}
+
